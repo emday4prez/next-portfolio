f05aadf5d3de053faa647b1a21362bf19fd56af0
Emerson <emerson.day@gmail.com>
2022-07-24 11:32:44 -0500

    created next/tailwind project

---
+++ pages/Photos.tsx
@@ -0,0 +1,21 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Photos: NextPage = () => {
+
+ return (
+  <div className="dark:bg-slate-900 flex min-h-screen flex-col items-center justify-center py-2">
+      <Head>
+        <title>Image Galleries</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+       
+      </main>
+  </div>
+ )
+}
+
+
+export default Photos;
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,35 +4,32 @@ import Image from 'next/image'
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="dark:bg-slate-900 flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>Emerson Day </title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
+      <main className="flex  dark:text-gray-100  white w-full flex-1 flex-col items-center justify-center px-20 text-center">
+        <h1 className="text-6xl font-bold text-slate-800 dark:text-slate-200">
+          Emerson Day
+         
         </h1>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
+        <p className="mt-6 text-2xl">
+          <code className="rounded-md bg-gray-200 dark:bg-gray-700 dark:text-emerald-400 p-3 font-mono text-xl">
+            full stack developer
           </code>
         </p>
 
         <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
           <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
+            href="/projects"
+            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-emerald-600 focus:text-emerald-600"
           >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
+            <h3 className="text-2xl font-bold text-emerald-800 dark:text-emerald-200">Projects &rarr;</h3>
             <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
+              Find in-depth information about tools I use.
             </p>
           </a>
 
@@ -40,29 +37,29 @@ const Home: NextPage = () => {
             href="https://nextjs.org/learn"
             className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
           >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
+            <h3 className="text-2xl font-bold text-blue-800 dark:text-blue-200">Education &rarr;</h3>
             <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
+              Schools I have attended, and courses I have completed. 
             </p>
           </a>
 
           <a
             href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
+            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-amber-600 focus:text-amber-600"
           >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
+            <h3 className="text-2xl font-bold text-amber-800 dark:text-amber-200">Photo Galleries &rarr;</h3>
             <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
+              Discover different styles of photo galleries I have designed.
             </p>
           </a>
 
           <a
             href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
+            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-rose-600 focus:text-rose-600"
           >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
+            <h3 className="text-2xl font-bold text-rose-800 dark:text-rose-200">Fun &rarr;</h3>
             <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              Having fun with APIs.
             </p>
           </a>
         </div>
@@ -75,7 +72,7 @@ const Home: NextPage = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          {' '}
           <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
         </a>
       </footer>
